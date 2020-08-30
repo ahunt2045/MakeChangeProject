485f96e5d1fde7f6519c0a1f94ff4990b5c7c277
Andrew <ahunt2045@gmail.com>
2020-08-30 09:56:28 -0600

    finished program

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
 ## Make Change Project
+1st week Java Project
+  -Cash Register
 
 ### Overview
+The user will required to enter two monetary amounts.
+There are two acceptable formats:
+  -00.00 with range 00.00 - 20.00
+  -0 with range 0-20
+The first monetary entry will be the item's price.
+The second monetary entry will be the amount tendered.
+The program will print to screen the difference,
+the customer's change, in correct denominations.
+EXAMPLE:
+Price of Item is 3.96
+Amount tendered is 20.00
+Program will print to screen:
+1 ten dollar bill, 1 five dollar bill, 1 one dollar bill, 4 pennies.
 
-this is where I'll put a summary of my project
 
 ### Topics
 * Taking User Input
 * Casting
 * Methods
+* Conditionals
+* Basic Arithmetic/Modulo operator
 
 #### How to Run
-
-1. Instructions
-2. More Instructions
-3. Even more Instructions
+1. The user will be prompted for two inputs
+  - The first input will be the item's price.
+  - The second input will be the amount tendered by user.
+2. Acceptable formats by user:
+  - 00.00 with range 00.00 to 20.00
+  - 0 with range 0 to 20    
+3. The program will print to screen the user's change in specific denominations
--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -1,10 +1,171 @@
 package makechange;
-
+import java.util.Scanner;
 public class MakeChange {
 
 	public static void main(String[] args) {
-System.out.println("Hello World");
-
-	}
-
+		double purchasedPrice = 0;
+		int transformedPurchasedPrice = 0;
+		double amountTendered = 0;
+		int transformedAmountTendered = 0;
+		int change = 0;
+		
+		//Intro to store
+		printIntroduction();
+		
+		//Getting user input
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Hmm...haven't sold one of those since the \"incident\", "
+				+ "\nbut I'm sure that one will work just fine. What's the price?");
+		//Cast double into integer
+		purchasedPrice = keyboard.nextDouble() * 100;
+		transformedPurchasedPrice = (int) purchasedPrice;
+		System.out.println("You look a little shady...you sure you got money?"
+				+ "\nHow much money do you have?");
+		//Cast double into integer		
+		amountTendered = keyboard.nextDouble() * 100;
+		transformedAmountTendered = (int) amountTendered;
+		change = transformedAmountTendered - transformedPurchasedPrice;
+	    
+		//Closing scanner because I am a good little programmer
+		keyboard.close();
+		
+		if (change > 0) {
+		//Practice calling methods
+		int numTens = getTens(change);
+		int numFives = getFives(change);	
+		int numOnes = getOnes(change);	
+		int numQuarters = getQuarters(change);	
+		int numDimes = getDimes(change);	
+		int numNickels = getNickels(change);	
+		int numPennies = getPennies(change);
+		printAnswer(numTens, numFives, numOnes, numQuarters, numDimes, numNickels, numPennies);
+		}
+		else if (change == 0) {
+			System.out.println("Exact change? What, no tip?...Lousy cheapskate...");
+		}
+		else {
+			System.out.println("I knew you were a bum the moment you walked in,"
+					+ "\nI'm running a business here, not a charity!"
+					+ "\nCome back when you have more green!"
+					+ "\nAnd next time wear a mask!");
+		}
+	}		
+		public static int getTens(int change) {
+			int tens = change/1000;
+			return tens;
+		}
+		public static int getFives(int change) {
+			int fives = (change % 1000) / 500;
+			return fives;
+		}
+		public static int getOnes(int change) {
+			int ones = ((change % 1000) % 500) / 100;
+			return ones;
+		}
+		public static int getQuarters(int change) {
+			int quarters = (((change % 1000) % 500) % 100) / 25;
+			return quarters;
+		}
+		public static int getDimes(int change) {
+			int dimes = ((((change % 1000) % 500) % 100) % 25) / 10;
+			return dimes;
+		}
+		public static int getNickels(int change) {
+			int nickels = (((((change % 1000) % 500) % 100) % 25) % 10) / 5;
+			return nickels;
+		}
+		public static int getPennies(int change) {
+			int pennies = ((((((change % 1000) % 500) % 100) % 25) % 10) % 5) / 1;
+			return pennies;
+		}
+		public static void printAnswer(int tens, int fives, int ones, int quarters, int dimes, int nickels, int pennies) {
+			System.out.println("And here I thought you were penniless, here is your change:");
+			if (tens > 0) {
+				if (fives > 0 || ones > 0 || quarters > 0 || dimes > 0 || nickels > 0 || pennies > 0) {
+					System.out.print(tens + " ten dollar bill, ");
+				}
+				else {
+				System.out.println(tens + " ten dollar bill.");
+				}
+			}
+			if (fives > 0) {
+				if (ones > 0 || quarters > 0 || dimes > 0 || nickels > 0 || pennies > 0) {
+					System.out.print(fives + " five dollar bill, ");
+				}
+				else {
+				System.out.println(fives + " five dollar bill.");
+				}
+			}
+			if (ones > 1) {
+				if (quarters > 0 || dimes > 0 || nickels > 0 || pennies > 0) {
+					System.out.print(ones + " one dollar bills, ");
+				}
+				else {
+					System.out.println(ones + " one dollar bills.");
+				}
+			}
+			if (ones == 1) {
+				if(quarters > 0 || dimes > 0 || nickels > 0 || pennies > 0) {
+					System.out.print(ones + " one dollar bill, ");
+				}
+				else {
+					System.out.println(ones + " one dollar bill.");
+				}
+			}
+			if (quarters > 1) {
+				if(dimes > 0 || nickels > 0 || pennies > 0) {
+					System.out.print(quarters + " quarters, ");
+				}
+				else {
+					System.out.println(quarters + " quarters.");
+				}
+			}
+			if (quarters == 1) {
+				if(dimes > 0 || nickels > 0 || pennies > 0) {
+					System.out.print(quarters + " quarter, ");
+				}
+				else {
+					System.out.println(quarters + " quarter.");
+				}
+			}
+			if (dimes > 1) {
+				if(nickels > 0 || pennies > 0) {
+					System.out.print(dimes + " dimes, ");
+				}
+				else {
+					System.out.println(dimes + " dimes.");
+				}
+			}
+			if (dimes == 1) {
+				if(nickels > 0 || pennies > 0) {
+					System.out.print(dimes + " dime, ");
+				}
+				else {
+					System.out.print(dimes + " dime.");
+				}
+			}
+			if (nickels > 0) {
+				if (pennies > 0) {
+					System.out.print(nickels + " nickel, ");
+				}
+				else {
+					System.out.print(nickels + " nickel. ");
+				}
+			}
+			if (pennies > 1) {
+				System.out.print(pennies + " pennies.");
+			}
+			if (pennies == 1) {
+			System.out.print(pennies + " penny.");
+			}
+		}
+		public static void printIntroduction() {
+			System.out.println("***   Welcome to Andrew's Discarded Crap -n Stuff ***");
+			System.out.println("***     Used Toiletries, Dirty Socks 20% off!     ***");
+			System.out.println("***     Big Ten Football Tickets Half-Priced!     ***");
+			System.out.println("***           --Cash Only, No Refunds--           ***");
+			System.out.println("***                --No Loitering--               ***");
+			System.out.println("***           --No Mask, No Service--             ***");
+			System.out.println("                                                     ");			
+		}
 }
